5e28c3afc4a210ff35ece2b4b178952380254cc5
kravcneger <kravcneger@gmail.com>
2021-05-24 03:14:04 +0400

    Update permissions in manifest.Change approach of annotation

--- code.js
+++ code.js
@@ -1,37 +1,64 @@
- 
-var actualCode = '(' + function() {
+ var actualCode = '(' + function() {
   var annotation_toogle = false;
 
-  function loadAnnotation(){
-    var data = lichess.analysis.data;
-    var treeParts =  data.treeParts;
-    for(var i=1; i<data.treeParts.length;i++ ){
-      if(treeParts[i]['eval']){
-        if( !annotation_toogle ){
-          if(treeParts[i]['eval']['cp'] != null){
-            treeParts[i]['san'] += "(" + ((treeParts[i]['eval']['cp'] / 100.0).toFixed(1)).toString() + ")";
-          }else if(treeParts[i]['eval']['mate'] != undefined) {
-            treeParts[i]['san'] += "(m " + treeParts[i]['eval']['mate'] + ")";
-          }
-        }else{
-          treeParts[i]['san'] = treeParts[i]['san'].replaceAll(/(\([m]?[ ]?[-]?[0-9]{1,3}\))/ig, "");
-          treeParts[i]['san'] = treeParts[i]['san'].replaceAll(/(\(.{1,8}\))/ig, "");
-        }
-      }
-    }
-    annotation_toogle = !annotation_toogle;
+  function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
   }
+  setTimeout(function(){
+    var el = document.createElement("p");
+    el.innerHTML = "";
+    el.setAttribute("aria-live", "assertive");
+    el.setAttribute("aria-atomic", "1");
+    el.classList.add("evalposition");
+    var div = document.querySelector('.position');
+    insertAfter(div, el);
+  }, 3800);
+
+  function loadAnnotation(){
+    var position_selector = document.querySelector('.position');
+    var evalposition = document.querySelector('.evalposition');
+    if(typeof position_selector == "undefined"){
+      return;
+    }
+
+    var positionObserver = new MutationObserver(function(records) {
+      if(annotation_toogle){
+        if(typeof lichess.analysis.node['eval'] != "undefined"){
+          if(typeof lichess.analysis.node['eval']['cp'] != "undefined"){
+            evalposition.innerHTML = ((lichess.analysis.node['eval']['cp'] / 100.0).toFixed(1)).toString();
+          }else if(typeof lichess.analysis.node['eval']['mate'] != "undefined") {
+            evalposition.innerHTML = "m " + lichess.analysis.node['eval']['mate'];
+          }else{
+            evalposition.innerHTML = "";
+          }
+        }
+      }else{
+        evalposition.innerHTML = "";
+      }
+    });
+    positionObserver.observe(position_selector, {
+      childList: true,
+      characterData: true,
+      subtree: true,
+      characterDataOldValue: true
+    });
+  }
+  setTimeout(loadAnnotation, 4000);
 
   document.addEventListener("keydown", function(e){
     if (e.shiftKey && e.code == 'KeyA') {
       if(typeof lichess !== "undefined" && typeof lichess.analysis !== "undefined" && typeof lichess.analysis.data !== "undefined" ){
-        loadAnnotation();
+        annotation_toogle = !annotation_toogle;
       }
     }
   });
 
-  function loadDuplicateMovesPatch(){
+  function fixDuplicateMove(){
     var last_move_selector = document.querySelector('.lastMove');
+    if(typeof last_move_selector == "undefined"){
+      return;
+    }
+
     var last_move = "";
     var mutateObserver = new MutationObserver(function(records) {
       if( last_move_selector.innerHTML.match(/[\.]/) ){
@@ -51,8 +78,9 @@ var actualCode = '(' + function() {
       subtree: true,
       characterDataOldValue: true
     });
+
   }
-  setTimeout(loadDuplicateMovesPatch, 5000);
+  setTimeout(fixDuplicateMove, 4000);
 
 } + ')()';
 
